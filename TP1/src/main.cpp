e357f28931921d752578715cb78783ce5016f545
sabrinasuellen1 <sabrinasuellen1@gmail.com>
2021-10-24 10:58:09 -0300

    Atualizacao main

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -14,81 +14,81 @@ using namespace std;
 int main () {
   /*
   */
-  long long int quantidade = 10;
+  long long int quantidade = 100000000;
     
-    Experimentos* experimentos= new Experimentos;
+  Experimentos* experimentos= new Experimentos;
 
-    long long int *vector = new long long int[quantidade];
-    long long int *vectorAux = new long long int[quantidade];
+  long long int *vector = new long long int[quantidade];
+  long long int *vectorAux = new long long int[quantidade];
 
-    gerador(vector, quantidade, 2);
-    ClonarVetor(vector, vectorAux, quantidade);
+  gerador(vector, quantidade, 3);
+  ClonarVetor(vector, vectorAux, quantidade);
 
-    cout <<"Testando o MergeSort..."<<endl;
-    clock_t c2, c1; /* variáveis que contam ciclos do processador */
-    printVector(vector, quantidade);
-
-
-    c1=clock(); /* coloque aqui o código que você quer medir o tempo de execução */
-    mergeSort(vector, quantidade);
-    c2=clock();
-
-    experimentos->tempo=(c2-c1)*1000/CLOCKS_PER_SEC;/* agora tempo guarda o tempo de execução em milisegundos */
-        
-    cout<<"-----------------------------------------------";
-    cout<<"\nTempo de execucao...........: "<<experimentos->tempo<<" milissegundos";
-    cout<<"\nNumero de comparacoes.......: "<<experimentos->comparacao;
-    cout<<"\n-----------------------------------------------"<<endl;
-
-    printVector(vector, quantidade);
-
-    ClonarVetor(vectorAux, vector, quantidade);
-    cout<<"\n\n\nTestando o InsertionSort..."<<endl;
-    //clock_t c2, c1; /* variáveis que contam ciclos do processador */
-    
-    printVector(vector, quantidade);
-
-    c1=clock(); /* coloque aqui o código que você quer medir o tempo de execução */
-    insertionsort(vector, quantidade);
-    c2=clock();
-
-    experimentos->tempo=(c2-c1)*1000/CLOCKS_PER_SEC;/* agora tempo guarda o tempo de execução em milisegundos */
-
-    cout<<"-----------------------------------------------";
-    cout<<"\nTempo de execucao...........: "<<experimentos->tempo<<" milissegundos";
-    cout<<"\nNumero de comparacoes.......: "<<experimentos->comparacao;
-    cout<<"\n-----------------------------------------------"<<endl;
-    
-    printVector(vector, quantidade);
+  cout <<"Testando o MergeSort..."<<endl;
+  clock_t c2, c1; /* variáveis que contam ciclos do processador */
+  //printVector(vector, quantidade);
 
 
-    ClonarVetor(vectorAux, vector, quantidade);
-    cout<<"\n\n\nTestando o RadixSort..."<<endl;
-    //clock_t c2, c1; /* variáveis que contam ciclos do processador */
-    //printVector(vector, quantidade);
+  c1=clock(); /* coloque aqui o código que você quer medir o tempo de execução */
+  mergeSort(vector, quantidade);
+  c2=clock();
 
-    c1=clock(); /* coloque aqui o código que você quer medir o tempo de execução */
-    
-            
-    // Function Call
-    radixsort(vector, quantidade);
-    c2=clock();
+  experimentos->tempo=(c2-c1)*1000/CLOCKS_PER_SEC;/* agora tempo guarda o tempo de execução em milisegundos */
+      
+  cout<<"-----------------------------------------------";
+  cout<<"\nTempo de execucao...........: "<<experimentos->tempo<<" milissegundos";
+  cout<<"\nNumero de comparacoes.......: "<<experimentos->comparacao;
+  cout<<"\n-----------------------------------------------"<<endl;
 
-    experimentos->tempo=(c2-c1)*1000/CLOCKS_PER_SEC;/* agora tempo guarda o tempo de execução em milisegundos */
-        
+  //printVector(vector, quantidade);
+
+  ClonarVetor(vectorAux, vector, quantidade);
+  cout<<"\n\n\nTestando o InsertionSort..."<<endl;
+  //clock_t c2, c1; /* variáveis que contam ciclos do processador */
+  
+  //printVector(vector, quantidade);
+
+  c1=clock(); /* coloque aqui o código que você quer medir o tempo de execução */
+  insertionsort(vector, quantidade);
+  c2=clock();
+
+  experimentos->tempo=(c2-c1)*1000/CLOCKS_PER_SEC;/* agora tempo guarda o tempo de execução em milisegundos */
+
+  cout<<"-----------------------------------------------";
+  cout<<"\nTempo de execucao...........: "<<experimentos->tempo<<" milissegundos";
+  cout<<"\nNumero de comparacoes.......: "<<experimentos->comparacao;
+  cout<<"\n-----------------------------------------------"<<endl;
+  
+  //printVector(vector, quantidade);
 
 
-    cout<<"-----------------------------------------------";
-    cout<<"\nTempo de execucao...........: "<<experimentos->tempo<<" milissegundos";
-    cout<<"\nNumero de comparacoes.......: "<<experimentos->comparacao;
-    cout<<"\n-----------------------------------------------"<<endl;
+  ClonarVetor(vectorAux, vector, quantidade);
+  cout<<"\n\n\nTestando o RadixSort..."<<endl;
+  //clock_t c2, c1; /* variáveis que contam ciclos do processador */
+  //printVector(vector, quantidade);
 
-    printVector(vector, quantidade);
+  c1=clock(); /* coloque aqui o código que você quer medir o tempo de execução */
+  
+          
+  // Function Call
+  radixsort(vector, quantidade);
+  c2=clock();
+
+  experimentos->tempo=(c2-c1)*1000/CLOCKS_PER_SEC;/* agora tempo guarda o tempo de execução em milisegundos */
+      
 
 
-    delete[] vector;
-    delete[] vectorAux;
-    delete[] experimentos;
+  cout<<"-----------------------------------------------";
+  cout<<"\nTempo de execucao...........: "<<experimentos->tempo<<" milissegundos";
+  cout<<"\nNumero de comparacoes.......: "<<experimentos->comparacao;
+  cout<<"\n-----------------------------------------------"<<endl;
+
+  //printVector(vector, quantidade);
+
+
+  delete[] vector;
+  delete[] vectorAux;
+  delete[] experimentos;
   
   return 0;
 }
